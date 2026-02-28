d52e2dda0a32d60caacb407b912fbba6fd8d3a65
Dominic Opitz <dominic.opitz@gmail.com>
2026-02-28 16:27:19 +0100

    feat: update Package.swift to use binary URLs for VLCKit dependencies

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:6.0
 import PackageDescription
 
+let binaryBaseURL = "https://github.com/dooop/swift-vlc/releases/download/0.1.0/"
+
 let package = Package(
   name: "swift-vlc",
   defaultLocalization: "en",
@@ -52,15 +54,18 @@ let package = Package(
     ),
     .binaryTarget(
       name: "VLCKit",
-      path: "Frameworks/VLCKit.xcframework"
+      url: "\(binaryBaseURL)VLCKit.xcframework.zip",
+      checksum: "5b475cc6fa3c1d56d71af26efad247076690f608fd7fa103b33bf980736d5971"
     ),
     .binaryTarget(
       name: "MobileVLCKit",
-      path: "Frameworks/MobileVLCKit.xcframework"
+      url: "\(binaryBaseURL)MobileVLCKit.xcframework.zip",
+      checksum: "e34563249ac1b1045eaeff9a3172640e22e0e9f66b806b020d885bf7f346f4f3"
     ),
     .binaryTarget(
       name: "TVVLCKit",
-      path: "Frameworks/TVVLCKit.xcframework"
+      url: "\(binaryBaseURL)TVVLCKit.xcframework.zip",
+      checksum: "370b14b48b39591b11d009ba959e2c6d342034b774270b8d18de3591dc80884d"
     ),
   ]
 )
